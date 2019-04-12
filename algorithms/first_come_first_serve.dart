0cb7919428ca6b8c7f73dc1b7c5261f1d272df1b
Nikhil Anand <nikhil_anand@outlook.com>
2019-04-13 00:41:46 +0530

    modified first_come_first_serve, fixed execute function

--- algorithms/first_come_first_serve.dart
+++ algorithms/first_come_first_serve.dart
@@ -4,36 +4,40 @@ import '../models/process_queue.dart';
 
 class FCFS extends Algorithm {
   FCFS(List<Process> processList) : super.fromProcessList(processList) {
+    // sort the process list on the basis of arrival time
     sortProcessList('arrival');
+    
+    for (Process process in super.processList) {
+      arrivalTimeList.add(process.arrival_time);
+    }
     readyQueue = ProcessQueue();
     runningQueue = ProcessQueue();
   }
 
+  @override
   void Execute() {
-    int i = 0;
-    List<Process> toRemove = [];
-    for (Process process in processList) {
-      runningQueue.addProcessFromIterable(processList.where((Process process) {
-        if (process.arrival_time == i && process.burst_time != 0)
+    int current_time;
+    for (int arrival_time in arrivalTimeList) {
+      readyQueue.addProcessFromIterable(processList.where((Process process) {
+        if (process.arrival_time == arrival_time && process.burst_time != 0)
           return true;
         else
           return false;
       }));
-      toRemove.add(process);
-      i++;
     }
-    processList.removeWhere((Process process) => toRemove.contains(process));
-    i = 0;
+    runningQueue.copyProcessQueue(readyQueue);
+    current_time = 0;
     for (Process process in runningQueue) {
       if (runningQueue.isEmpty) print('empty');
-      i += process.burst_time;
-      process.completion_time = i;
+      current_time += process.burst_time;
+      process.completion_time = current_time;
       process.turnaround_time = process.completion_time - process.arrival_time;
       process.waiting_time = process.turnaround_time - process.burst_time;
     }
     processList = runningQueue.convertToList();
   }
 
+  /// Prints the Gantt Chart for the implemented algorithm
   void printGanttChart() {
     super.GanttChart(processList);
   }
