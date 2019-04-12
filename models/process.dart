8448f01e07d67cb91308daa8ff317b8da6405272
Nikhil Anand <nikhil_anand@outlook.com>
2019-04-13 00:36:46 +0530

    modified process class, added comments

--- models/process.dart
+++ models/process.dart
@@ -1,20 +1,21 @@
 class Process {
-  int process_id;
-  int arrival_time;
-  int burst_time;
-  int turnaround_time;
-  int waiting_time;
-  int completion_time;
+  int process_id; // process id of a process
+  int arrival_time; // arrival time of a process
+  int burst_time; // burst time of a process
+  int turnaround_time; // turnaround time of a process
+  int waiting_time; // waiting time of a process
+  int completion_time; // completion time of a process
 
   Process({this.process_id, this.arrival_time, this.burst_time});
 
-  Process.fromProcessInfo(List<String> processInfo){
+  Process.fromProcessInfo(List<String> processInfo) {
     process_id = int.parse(processInfo[0]);
     arrival_time = int.parse(processInfo[1]);
     burst_time = int.parse(processInfo[2]);
   }
 
-  void printProcessInfo(){
+  /// Print the process's detailed information
+  void printProcessInfo() {
     print('${process_id}\t${arrival_time}\t${burst_time}\t${turnaround_time}\t${waiting_time}');
   }
 
