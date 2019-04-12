9bf4798113047b4556f8b892412df6102b815546
Nikhil Anand <nikhil_anand@outlook.com>
2019-04-13 00:28:23 +0530

    modified algorithm class, fixed sort function

--- models/algorithm.dart
+++ models/algorithm.dart
@@ -3,23 +3,27 @@ import 'process_queue.dart';
 
 abstract class Algorithm {
   /// average turnaround time taken by the algorithm
-  int avg_turnaround_time;
-
+  double avg_turnaround_time = 0.0;
   /// average waiting time taken by the algorithm
-  int avg_waiting_time;
+  double avg_waiting_time = 0.0;
 
   /// contains processes ready for execution by CPU
   ProcessQueue readyQueue;
-
   /// contains processes executed by CPU
   ProcessQueue runningQueue;
 
   /// processes to be scheduled by OS
   List<Process> processList;
 
+  /// arrival time of all the processes
+  List<int> arrivalTimeList;
+
   Algorithm();
 
-  Algorithm.fromProcessList(this.processList);
+  Algorithm.fromProcessList(List<Process> processList) {
+    this.processList = List.from(processList);
+    this.arrivalTimeList = [];
+  }
 
   /// sort process list on the basis of `'arrival', 'burst', 'both'`
   bool sortProcessList(String sortBasis) {
@@ -72,5 +76,10 @@ abstract class Algorithm {
       process.printProcessInfo();
     });
     print('*******************************************');
+  }
+
+  /// executes the algorithm to schedule the processes in the system
+  void Execute(){
+    return null;
   }
 }
