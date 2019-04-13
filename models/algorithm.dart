3ec492cd7db5919a196b4b1b8dd9f57e4c98cee5
Nikhil Anand <nikhil_anand@outlook.com>
2019-04-13 14:56:35 +0530

    modified algorithm class, refactored sort function

--- models/algorithm.dart
+++ models/algorithm.dart
@@ -1,14 +1,17 @@
 import 'process.dart';
 import 'process_queue.dart';
+import 'comparable.dart';
 
 abstract class Algorithm {
   /// average turnaround time taken by the algorithm
   double _avg_turnaround_time = 0.0;
+
   /// average waiting time taken by the algorithm
   double _avg_waiting_time = 0.0;
 
   /// contains processes ready for execution by CPU
   ProcessQueue readyQueue;
+
   /// contains processes executed by CPU
   ProcessQueue runningQueue;
 
@@ -33,40 +36,13 @@ abstract class Algorithm {
   bool sortProcessList(String sortBasis) {
     // check basis on which sorting will be performed
     if (sortBasis == 'arrival') {
-      processList.sort((Process a, Process b) {
-        if (a.arrival_time < b.arrival_time)
-          return -1;
-        else if (a.arrival_time == b.arrival_time) {
-          if (a.process_id > b.process_id)
-            return 1;
-          else
-            return 0;
-        } else
-          return 1;
-      });
+      processList.sort(compareOnArrival);
       return true;
     } else if (sortBasis == 'burst') {
-      processList.sort((Process a, Process b) {
-        if (a.burst_time < b.burst_time)
-          return -1;
-        else if (a.burst_time == b.burst_time) {
-          return 0;
-        } else
-          return 1;
-      });
+      processList.sort(compareOnBurst);
       return true;
     } else if (sortBasis == 'both') {
-      processList.sort((Process a, Process b) {
-        if (a.arrival_time < b.arrival_time)
-          return -1;
-        else if (a.arrival_time == b.arrival_time) {
-          if (a.burst_time > b.burst_time)
-            return 1;
-          else
-            return 0;
-        } else
-          return 1;
-      });
+      processList.sort(compareOnArrivalAndBurst);
       return true;
     } else
       return false;
@@ -88,21 +64,22 @@ abstract class Algorithm {
     return (_avg_waiting_time /= processList.length);
   }
 
-
   /// generate Gantt Chart for the given process list
-  void GanttChart(List<Process> processList) {
-    print('************************* Gantt Chart *************************');
-    print('P_ID\tP_AT\tP_BT\tP_TAT\tP_WT');
-    processList.forEach((Process process) {
-      process.printProcessInfo();
-    });
-    print('***************************************************************');
-    print('Avg Turnaround Time = ${this.avg_turnaround_time}\tAvg Waiting Time = ${this.avg_waiting_time}');
-    print('***************************************************************');
+  void printGanttChart() {
+    if (processList.isNotEmpty) {
+      print('************************* Gantt Chart *************************');
+      print('P_ID\tP_AT\tP_BT\tP_TAT\tP_WT');
+      processList.forEach((Process process) {
+        process.printProcessInfo();
+      });
+      print('***************************************************************');
+      print('Avg Turnaround Time = ${this.avg_turnaround_time}\tAvg Waiting Time = ${this.avg_waiting_time}');
+      print('***************************************************************');
+    }
   }
 
   /// executes the algorithm to schedule the processes in the system
-  void Execute(){
+  void Execute() {
     return null;
   }
 }
